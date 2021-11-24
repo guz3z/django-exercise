ababb53fb6a2a9b207e7ffaf8636bd7671d05cb6
“yourGithubUsername” <your@emailadd.com>
2021-11-24 18:59:20 +0000

    add url patterns

--- django-exercise/museum/library/urls.py
+++ django-exercise/museum/library/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="library-home")
+    path('', views.home, name="library-home"),
+    path('library/', views.home, name="library-home")
 ]
