589c5122acd3d7f786a7d4a8377302e9163d9b1c
“yourGithubUsername” <your@emailadd.com>
2021-11-24 19:14:06 +0000

    add books views

--- django-exercise/museum/library/views.py
+++ django-exercise/museum/library/views.py
@@ -8,3 +8,9 @@ def home(request):
 
 def library(request):
     return HttpResponse("<h1>This is the library page</h1>")
+
+def books(request):
+    return HttpResponse(
+        "<h1>These are all our books</h1>"
+        f"<p>We have {len(books)} books in our library</p>"
+    )
